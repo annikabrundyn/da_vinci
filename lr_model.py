35fe8f82f3c07f48893308b7afa6e5f8e64aefb3
Annika Brundyn <annikabrundyn1@gmail.com>
2020-10-30 20:37:49 -0400

    right view model

--- lr_model.py
+++ lr_model.py
@@ -1,60 +1,26 @@
 from argparse import ArgumentParser, Namespace
 
 import pytorch_lightning as pl
-import torch
-import torch.nn.functional as F
 import torchvision
 
 from unet import UNet
 from data import DaVinciDataModule
 from model import DepthMap
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-from pytorch_lightning.metrics.functional import ssim, psnr
-
 
 class LeftRightDepthMap(DepthMap):
 
-    def __init__(
-            self,
-            lr: float = 0.001,
-            num_classes: int = 1,
-            frames_per_sample: int = 5,
-            frames_to_drop: int = 2,
-            num_layers: int = 5,
-            features_start: int = 64,
-            bilinear: bool = False,
-            output_img_freq: int = 100,
-            batch_size: int = 16,
-            **kwargs
-    ):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.net = UNet(num_classes=num_classes,
+        self.net = UNet(num_classes=self.num_classes,
                         input_channels=2*self.input_channels,
                         num_layers=self.num_layers,
                         features_start=self.features_start,
                         bilinear=self.bilinear)
 
-    def _log_images(self, img, target, pred, step_name, limit=1):
-        # TODO: Randomly select image from batch
-        img = img[:limit]
-        target = target[:limit]
-        pred = pred[:limit]
-
-        # Log input/original image
-        img = img.permute(1, 0, 2, 3)
-        input_images = torchvision.utils.make_grid(img, nrow=self.input_channels)
-        self.logger.experiment.add_image(f'{step_name}_input_img', input_images, self.trainer.global_step)
-
-        # Log colorized depth maps - using magma colormap
-        color_target_dm = self._matplotlib_imshow(target)
-        color_pred_dm = self._matplotlib_imshow(pred)
-
-        self.logger.experiment.add_figure(f'{step_name}_target_dm_color', color_target_dm, self.trainer.global_step)
-        self.logger.experiment.add_figure(f'{step_name}_pred_dm_color', color_pred_dm, self.trainer.global_step)
+    def _log_images(self, *args, **kwargs):
+        super()._log_images(*args, **kwargs, nrow=self.input_channels)
 
 
 if __name__ == '__main__':
@@ -67,7 +33,7 @@ if __name__ == '__main__':
     parser = pl.Trainer.add_argparse_args(parser)
 
     # model args
-    parser = DepthMap.add_model_specific_args(parser)
+    parser = LeftRightDepthMap.add_model_specific_args(parser)
     args = parser.parse_args()
 
     # data
@@ -84,7 +50,6 @@ if __name__ == '__main__':
 
     # model
     model = LeftRightDepthMap(**args.__dict__)
-    #model._log_images()
 
     # train
     trainer = pl.Trainer().from_argparse_args(args)
--- model.py
+++ model.py
@@ -17,13 +17,13 @@ from pytorch_lightning.metrics.functional import ssim, psnr
 class DepthMap(pl.LightningModule):
     def __init__(
             self,
-            lr: float = 0.001,
+            frames_per_sample: int,
+            frames_to_drop: int,
             num_classes: int = 1,
-            frames_per_sample: int = 5,
-            frames_to_drop: int = 2,
             num_layers: int = 5,
             features_start: int = 64,
             bilinear: bool = False,
+            lr: float = 0.001,
             output_img_freq : int = 100,
             batch_size : int = 16,
             **kwargs
@@ -31,9 +31,9 @@ class DepthMap(pl.LightningModule):
 
         super().__init__()
 
-        self.num_classes = num_classes
         self.frames_per_sample = frames_per_sample
         self.frames_to_drop = frames_to_drop
+        self.num_classes = num_classes
         self.num_layers = num_layers
         self.features_start = features_start
         self.bilinear = bilinear
@@ -105,7 +105,7 @@ class DepthMap(pl.LightningModule):
         plt.imshow(npimg, cmap=cmap)
         return fig
 
-    def _log_images(self, img, target, pred, step_name, limit=1):
+    def _log_images(self, img, target, pred, step_name, nrow=1, limit=1):
         # TODO: Randomly select image from batch
         img = img[:limit]
         target = target[:limit]
@@ -113,7 +113,7 @@ class DepthMap(pl.LightningModule):
 
         # Log input/original image
         img = img.permute(1, 0, 2, 3)
-        input_images = torchvision.utils.make_grid(img, nrow=1)
+        input_images = torchvision.utils.make_grid(img, nrow=nrow, padding=3)
         self.logger.experiment.add_image(f'{step_name}_input_img', input_images, self.trainer.global_step)
 
         # Log colorized depth maps - using magma colormap
@@ -126,9 +126,9 @@ class DepthMap(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument("--data_dir", type=str, default='.', help="path to nyu depth data")
-        parser.add_argument("--frames_per_sample", type=int, default=5, help="number of frames to include in each sample")
-        parser.add_argument("--frames_to_drop", type=int, default=2, help="number of frames to randomly drop in each sample")
+        parser.add_argument("--data_dir", type=str, help="path to davinci data")
+        parser.add_argument("--frames_per_sample", type=int, help="number of frames to include in each sample")
+        parser.add_argument("--frames_to_drop", type=int, help="number of frames to randomly drop in each sample")
         parser.add_argument("--num_classes", type=int, default=1, help="output channels")
         parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
         parser.add_argument("--output_img_freq", type=int, default=100)
