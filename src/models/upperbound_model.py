ff7fa97fcdc25a35739fd658684d76c54fd44b02
Annika Brundyn <annikabrundyn1@gmail.com>
2020-11-20 11:44:58 -0500

    saving working

--- src/models/upperbound_model.py
+++ src/models/upperbound_model.py
@@ -25,11 +25,30 @@ from models.model import DepthMap
 class UpperBoundImgCallback(Callback):
 
     def __init__(self, dl, epoch_logging_freq: int = 50):
-        # save every 50 epochs
+        # save predictions every 50 epochs
         self.epoch_logging_freq = epoch_logging_freq
         self.dl = dl
 
+    def on_sanity_check_end(self, trainer, pl_module):
+        '''save the input + target images only once'''
+        print("do this only once")
+        batch_idx = 0
+        for img, target, extra in self.dl:
+            folder_name = extra['image_set'][0]
+            frame_nums = extra['frame_nums'][0]
+
+            dir = trainer.checkpoint_callback.dirpath
+            dir = os.path.split(dir)[0]
+            inputs_dir_path = os.path.join(dir, "inputs")
+            targets_dir_path = os.path.join(dir, "targets")
+
+            pl_module._matplotlib_imshow_input_imgs(img.squeeze(0), folder_name, frame_nums, save_fig=True, title=f"input_{batch_idx}", dir_path=inputs_dir_path)
+            pl_module._matplotlib_imshow_dm(target.squeeze(0), title=f"target_{batch_idx}", save_fig=True, dir_path=targets_dir_path)
+
+            batch_idx += 1
+
     def on_validation_epoch_end(self, trainer, pl_module):
+        '''save only the predicted '''
         print('a')
         if trainer.current_epoch % self.epoch_logging_freq == 0:
             batch_idx = 0
@@ -44,12 +63,9 @@ class UpperBoundImgCallback(Callback):
                 dir = os.path.split(dir)[0]
                 dir_path = os.path.join(dir, f"epoch_{trainer.current_epoch}", "pred")
 
-                #pl_module._matplotlib_imshow_input_imgs(img.squeeze(0), folder_name, frame_nums, save_fig=True, title=f"input_{batch_idx}")
-                #pl_module._matplotlib_imshow_dm(target.squeeze(0), title=f"target_{batch_idx}", save_fig=True, location="target")
                 pl_module._matplotlib_imshow_dm(pred.squeeze(0), title=f"prediction_{batch_idx}", save_fig=True, dir_path=dir_path)
 
                 batch_idx += 1
-
 
 
 class UpperBoundModel(DepthMap):
@@ -79,7 +95,7 @@ class UpperBoundModel(DepthMap):
                 self.input_channels = 3 * self.input_channels
 
         else:
-            raise ValueError("This model currently only works with single frames")
+            raise ValueError("This model currently only works with single frames (color or b/w)")
 
     def training_step(self, batch, batch_idx):
         img, target, extra_info = batch
@@ -115,7 +131,7 @@ class UpperBoundModel(DepthMap):
         self.log('valid_ssim', ssim_val)
         self.log('valid_psnr', psnr_val)
 
-    def _matplotlib_imshow_input_imgs(self, img, folder_name, frame_nums, save_fig=False, title=None):
+    def _matplotlib_imshow_input_imgs(self, img, folder_name, frame_nums, save_fig=False, title=None, dir_path=None):
         """Summary
 
         Args:
@@ -152,9 +168,6 @@ class UpperBoundModel(DepthMap):
             ax.set_title(f"{side} view: {folder_name}/{frame_nums}")
 
         if save_fig:
-            dir = trainer.checkpoint_callback.dirpath
-            dir = os.path.split(dir)[0]
-            dir_path = os.path.join(dir, f"epoch_{self.current_epoch}", "input")
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
 
@@ -190,6 +203,7 @@ if __name__ == '__main__':
                            extra_info=True,
                            batch_size=args.batch_size,
                            num_workers=args.num_workers)
+    dm.setup()
     print("dm setup")
 
     # sanity check
