adb60d88bdbb5d361e18a68c5c3fd87df34dac12
Annika Brundyn <annikabrundyn1@gmail.com>
2020-11-20 15:39:32 -0500

    final

--- src/models/depth_map/upperbound_model.py
+++ src/models/depth_map/upperbound_model.py
@@ -21,7 +21,7 @@ from pytorch_lightning.callbacks import Callback
 
 class UpperBoundImgCallback(Callback):
 
-    def __init__(self, dl, epoch_logging_freq: int = 50):
+    def __init__(self, dl, epoch_logging_freq: int = 30):
         # save predictions every 50 epochs
         self.epoch_logging_freq = epoch_logging_freq
         self.dl = dl
@@ -47,7 +47,7 @@ class UpperBoundImgCallback(Callback):
     def on_validation_epoch_end(self, trainer, pl_module):
         '''save only the predicted '''
 
-        if trainer.current_epoch + 1 % self.epoch_logging_freq == 0:
+        if trainer.current_epoch % self.epoch_logging_freq == 0:
             print('save predicted val images')
             batch_idx = 0
             for img, target, extra in self.dl:
@@ -60,20 +60,21 @@ class UpperBoundImgCallback(Callback):
                 pl_module._matplotlib_imshow_dm(pred.squeeze(0), title=f"prediction_{batch_idx}", save_fig=True, dir_path=dir_path)
                 batch_idx += 1
 
-    def on_keyboard_interrupt(self, trainer, pl_module):
-        '''save predicted when kill experiment'''
-        print('save checkpoint val images')
-
-        batch_idx = 0
-        for img, target, extra in self.dl:
-            img, target = img.to(pl_module.device), target.to(pl_module.device)
-            pred = pl_module(img)
-
-            dir = os.path.split(trainer.checkpoint_callback.dirpath)[0]
-            dir_path = os.path.join(dir, f"epoch_{trainer.current_epoch + 1}", "pred")
-
-            pl_module._matplotlib_imshow_dm(pred.squeeze(0), title=f"prediction_{batch_idx}", save_fig=True, dir_path=dir_path)
-            batch_idx += 1
+    # this doesn't work on grid
+    # def on_keyboard_interrupt(self, trainer, pl_module):
+    #     '''save predicted when kill experiment'''
+    #     print('save checkpoint val images')
+    #
+    #     batch_idx = 0
+    #     for img, target, extra in self.dl:
+    #         img, target = img.to(pl_module.device), target.to(pl_module.device)
+    #         pred = pl_module(img)
+    #
+    #         dir = os.path.split(trainer.checkpoint_callback.dirpath)[0]
+    #         dir_path = os.path.join(dir, f"epoch_{trainer.current_epoch + 1}", "pred")
+    #
+    #         pl_module._matplotlib_imshow_dm(pred.squeeze(0), title=f"prediction_{batch_idx}", save_fig=True, dir_path=dir_path)
+    #         batch_idx += 1
 
 
 class UpperBoundModel(DepthMap):
