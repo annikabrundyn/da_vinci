3b2df9dbb7a2f6a25d9c01b786239598304ec429
Annika Brundyn <annikabrundyn1@gmail.com>
2020-11-22 14:56:44 -0500

    direct

---
+++ src/models/right_view/direct_right_model.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn.functional as F
+
+import os.path
+import numpy as np
+
+from argparse import ArgumentParser
+
+import pytorch_lightning as pl
+from pytorch_lightning.metrics.functional import ssim, psnr
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+from models.right_view.base_model import BaseRightModel
+from data.right_data import RightDaVinciDataModule
+from models.callbacks.callback_right_view import RightCallback
+from models.callbacks.fid_callback import FidCallback
+from models.right_view.right_unet import RightUNet
+
+
+if __name__ == "__main__":
+    # sets seed for numpy, torch, python.random and PYTHONHASHSEED
+    print("start right direct model")
+    pl.seed_everything(42)
+
+    parser = ArgumentParser()
+
+    # trainer args
+    parser = pl.Trainer.add_argparse_args(parser)
+
+    # model args
+    parser = BaseRightModel.add_model_specific_args(parser)
+    args = parser.parse_args()
+
+    # data
+    dm = RightDaVinciDataModule(
+        args.data_dir,
+        frames_per_sample=args.frames_per_sample,
+        frames_to_drop=args.frames_to_drop,
+        is_color_input=args.is_color_input,
+        is_color_output=args.is_color_output,
+        extra_info=True,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
+    dm.setup()
+    print("dm setup")
+
+    # sanity check
+    print("size of trainset:", len(dm.train_samples))
+    print("size of validset:", len(dm.val_samples))
+    print("size of testset:", len(dm.test_samples))
+
+    img, target, extra_info = next(iter(dm.train_dataloader()))
+    print(img.shape)
+    print(target.shape)
+    print(len(extra_info))
+
+    # model
+    model = BaseRightModel(**args.__dict__)
+    print("model instance created")
+    print("lightning version", pl.__version__)
+
+    # train
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=[RightCallback(dm.vis_img_dataloader()),FidCallback(dm.train_dataloader(),dm.val_dataloader())])
+    print("trainer created")
+    trainer.fit(model, dm.train_dataloader(), dm.val_dataloader())
