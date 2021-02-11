c4911cca1a66fba38a2755280bb39e31114bb6f6
Annika Brundyn <annikabrundyn1@gmail.com>
2021-02-11 12:29:50 -0500

    add residual connection

---
+++ src/models/right_view/unet_add_connect.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class UNet(nn.Module):
+    """
+    Args:
+        num_classes: Number of output classes required
+        num_layers: Number of layers in each side of U-net (default 5)
+        features_start: Number of features in first layer (default 64)
+        bilinear (bool): Whether to use bilinear interpolation or transposed convolutions (default) for upsampling.
+    """
+    def __init__(
+            self,
+            num_classes: int,
+            input_channels: int,
+            num_stack_horizontal: int = 1,
+            num_layers: int = 5,
+            features_start: int = 64,
+            bilinear: bool = False
+    ):
+        super().__init__()
+        self.num_layers = num_layers
+        self.input_channels = input_channels
+
+        layers = [DoubleConv(self.input_channels, features_start)]
+
+        feats = features_start
+        for _ in range(num_layers - 1):
+            layers.append(Down(feats, feats * 2))
+            feats *= 2
+
+        for _ in range(num_layers - 1):
+            layers.append(Up(feats, feats // 2, bilinear))
+            feats //= 2
+
+        layers.append(nn.Conv2d(feats, num_classes, kernel_size=1))
+
+        self.layers = nn.ModuleList(layers)
+
+        # WIP: add additional residual connection straight from input to output
+        self.final_conv = nn.Conv2d(2 * input_channels, input_channels, kernel_size=1)
+
+    def forward(self, x):
+        xi = [self.layers[0](x)]
+        # Down path
+        for layer in self.layers[1:self.num_layers]:
+            xi.append(layer(xi[-1]))
+        # Up path
+        for i, layer in enumerate(self.layers[self.num_layers:-1]):
+            xi[-1] = layer(xi[-1], xi[-2 - i])
+        # Final conv layer of original UNet
+        orig_output = self.layers[-1](xi[-1])
+
+        # WIP: add additional connection straight from input to output
+        input_concat_output = torch.cat([orig_output, x], dim=1)
+        new_output = self.final_conv(input_concat_output)
+
+        return orig_output, new_output
+
+
+class DoubleConv(nn.Module):
+    """
+    [ Conv2d => BatchNorm (optional) => ReLU ] x 2
+    """
+
+    def __init__(self, in_ch: int, out_ch: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Down(nn.Module):
+    """
+    Downscale with MaxPool => DoubleConvolution block
+    """
+
+    def __init__(self, in_ch: int, out_ch: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            DoubleConv(in_ch, out_ch)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Up(nn.Module):
+    """
+    Upsampling (by either bilinear interpolation or transpose convolutions)
+    followed by concatenation of feature map from contracting path, followed by DoubleConv.
+    """
+
+    def __init__(self, in_ch: int, out_ch: int, bilinear: bool = False):
+        super().__init__()
+        self.upsample = None
+        if bilinear:
+            self.upsample = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+                nn.Conv2d(in_ch, in_ch // 2, kernel_size=1),
+            )
+        else:
+            self.upsample = nn.ConvTranspose2d(in_ch, in_ch // 2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.upsample(x1)
+
+        # Pad x1 to the size of x2
+        diff_h = x2.shape[2] - x1.shape[2]
+        diff_w = x2.shape[3] - x1.shape[3]
+
+        x1 = F.pad(x1, [diff_w // 2, diff_w - diff_w // 2, diff_h // 2, diff_h - diff_h // 2])
+
+        # Concatenate along the channels axis
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+print("hi")
+model = UNet(num_classes=3, input_channels=3)
+x = torch.rand(1, 3, 384, 192)
+xhat = model(x)
+print("done")
