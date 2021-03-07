af156b05db959deb950ec951a164547b7f6b1c3d
Annika Brundyn <annikabrundyn1@gmail.com>
2021-03-06 20:50:31 -0500

    add fid, remove bad combine fns

--- src/models/right_view/combine_fns.py
+++ src/models/right_view/combine_fns.py
@@ -1,21 +1,21 @@
 import torch.nn as nn
 
-# class CombineConv3D(nn.Module):
-#     """
-#     Combine multiple embeddings using a 3D CNN
-#     """
-#
-#     def __init__(self, in_ch:int, out_ch: int = 1, kernel_size: int = 3, stride: int = 1, padding: int = 1):
-#         super().__init__()
-#         # in channels is num_frames
-#         self.net = nn.Conv3d(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
-#
-#     def forward(self, x):
-#         x = self.net(x)
-#
-#         # [b, 1, d1, d2, d3] --> [b, d1, d2, d3]
-#         x = x.squeeze(1)
-#         return x
+class CombineConv3D(nn.Module):
+    """
+    Combine multiple embeddings using a 3D CNN
+    """
+
+    def __init__(self, in_ch:int, out_ch: int = 1, kernel_size: int = 3, stride: int = 1, padding: int = 1):
+        super().__init__()
+        # in channels is num_frames
+        self.net = nn.Conv3d(in_ch, out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
+
+    def forward(self, x):
+        x = self.net(x)
+
+        # [b, 1, d1, d2, d3] --> [b, d1, d2, d3]
+        x = x.squeeze(1)
+        return x
 #
 #
 # class CombineConv1DAvgPool(nn.Module):
