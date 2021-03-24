b2246162a994e48290c3a9cbaf1c0e006655f36e
Jesse Swanson <js11133@nyu.edu>
2021-03-24 14:21:19 -0400

    add options for padding, specify frames, not adding input and targs

--- visualize.py
+++ visualize.py
@@ -9,19 +9,28 @@ IMAGE_WIDTH = 384
 IMAGE_HEIGHT = 192
 
 
-def concat_images(image_paths, size, shape, exp_paths):
+def concat_images(image_paths, size, shape, exp_paths, padding=3, include_inputs_and_targets=False, imgs=None):
     # Open images and resize them
     width, height = size
     images = map(Image.open, image_paths)
     images = [ImageOps.fit(image, size)
               for image in images]
 
-    # Create canvas for the final image with total size
-    shape = shape if shape else (1, len(images))
-    image_size = (width * (shape[1]), height * (shape[0]))
-    image = Image.new('RGB', image_size)
+    num_x = shape[0]
+    num_y = shape[1]
 
-    exp_paths = ["inputs", "targets"] + exp_paths
+    if include_inputs_and_targets:
+        exp_paths = ["inputs", "targets"] + exp_paths
+        num_y += 2
+
+    if imgs:
+        num_x = len(imgs) + 1
+
+        # Create canvas for the final image with total size
+    shape = shape if shape else (1, len(images))
+    image_size = (width * num_x) + (padding * (num_x -
+                                               1)), (height * num_y) + (padding * (num_y - 1))
+    image = Image.new('RGB', image_size)
 
     # Paste images into final image
     for row, name in zip(range(shape[0]), exp_paths):
@@ -30,21 +39,34 @@ def concat_images(image_paths, size, shape, exp_paths):
         img = Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT), "white")
         d = ImageDraw.Draw(img)
         font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", size=50)
-        if row == 0:
-            msg = "input"
-        elif row == 1:
-            msg = "target"
+
+        if include_inputs_and_targets:
+            if row == 0:
+                msg = "input"
+            elif row == 1:
+                msg = "target"
+            else:
+                msg = name.split('/')[-2]
         else:
             msg = name.split('/')[-2]
+
         w, h = d.textsize(msg)
         d.text((5, IMAGE_HEIGHT/2 - 20), msg, font=font, fill="black")
-        image.paste(img, (0, height * row))
+        image.paste(img, (0, (height + padding) * row))
 
         # Paste images
-        for col in range(1, shape[1]):
-            offset = width * col, height * row
-            idx = row * (shape[1]-1) + col
-            image.paste(images[idx-1], offset)
+        if imgs:
+            img_col = 1
+            for img in imgs:
+                offset = (width+padding) * img_col, (height + padding) * row
+                idx = row * (num_x-1) + img
+                image.paste(images[idx], offset)
+                img_col += 1
+        else:
+            for col in range(1, num_x):
+                offset = (width+padding) * col, (height + padding) * row
+                idx = row * (num_x-1) + col
+                image.paste(images[idx-1], offset)
 
     return image
 
@@ -54,6 +76,11 @@ if __name__ == "__main__":
     parser.add_argument('--exps', metavar='N', type=str, nargs='+',
                         help='experiment names', required=True)
     parser.add_argument("--exp_dir", type=str, required=True, help="path to davinci data")
+    parser.add_argument("--padding", type=int, default=3, required=False, help="padding for images")
+    parser.add_argument("--inputs_and_targets", action='store_true',
+                        help="Include inputs and targets")
+    parser.add_argument("--imgs", type=int, nargs='+', default=[],
+                        help="List of images to visualize")
     args = parser.parse_args()
 
     set_exps = set(args.exps)
@@ -68,35 +95,41 @@ if __name__ == "__main__":
                         path) == ["version_0", ".DS_Store"]
                     exp_paths.append(os.path.join(path, "version_0"))
 
-    # Generate list of image paths for inputs
     image_paths = []
-    inputs_fol = os.path.join(args.exp_dir, "inputs")
-    for fname in sorted(os.listdir(inputs_fol)):
-        if fname == ".DS_Store":
-            continue
-        path = os.path.join(inputs_fol, fname)
-        if os.path.isdir(path):
-            max_num = max(int(x[:6]) for x in os.listdir(path) if x != ".DS_Store")
-            image_paths.append(os.path.join(path, format(max_num, '06d') + ".png"))
+    if args.inputs_and_targets:
+        # Generate list of image paths for inputs
+        inputs_fol = os.path.join(args.exp_dir, "inputs")
+        for fname in sorted(os.listdir(inputs_fol)):
+            if fname == ".DS_Store":
+                continue
+            path = os.path.join(inputs_fol, fname)
+            if os.path.isdir(path):
+                max_num = max(int(x[:6]) for x in os.listdir(path) if x != ".DS_Store")
+                image_paths.append(os.path.join(path, format(max_num, '06d') + ".png"))
 
-    # Generate list of image paths for targets
-    targets_fol = os.path.join(args.exp_dir, "targets")
-    image_paths.extend([os.path.join(targets_fol, f)
-                        for f in sorted(os.listdir(targets_fol)) if f.endswith('.png')])
+        # Generate list of image paths for targets
+        targets_fol = os.path.join(args.exp_dir, "targets")
+        image_paths.extend([os.path.join(targets_fol, f)
+                            for f in sorted(os.listdir(targets_fol)) if f.endswith('.png')])
 
     # Generate list of images in the experiment paths (use the last epoch)
     for top_exp_folder in exp_paths:
         dirs = [fname for fname in os.listdir(top_exp_folder) if (
             os.path.isdir(os.path.join(top_exp_folder, fname)) and "epoch" in fname)]
+
         max_epoch = max(int(x[6:]) for x in dirs)
         max_epoch_folder = "epoch_" + str(max_epoch)
 
-        image_paths.extend([os.path.join(top_exp_folder, max_epoch_folder, f)
-                            for f in sorted(os.listdir(os.path.join(top_exp_folder, max_epoch_folder))) if f.endswith('.png')])
+        if args.imgs:
+            image_paths.extend([os.path.join(top_exp_folder, max_epoch_folder, f)
+                                for f in [sorted(os.listdir(os.path.join(top_exp_folder, max_epoch_folder)))[i] for i in args.imgs] if f.endswith('.png')])
+        else:
+            image_paths.extend([os.path.join(top_exp_folder, max_epoch_folder, f)
+                                for f in sorted(os.listdir(os.path.join(top_exp_folder, max_epoch_folder))) if f.endswith('.png')])
 
     # Create and save image grid
     img = concat_images(image_paths, (IMAGE_WIDTH, IMAGE_HEIGHT),
-                        (len(exp_paths)+2, 14+1), exp_paths)
+                        (14+1, len(exp_paths)), exp_paths, args.padding, args.inputs_and_targets, args.imgs)
 
     img.show()
     img.save('_vs_'.join(set_exps)+'.png')
