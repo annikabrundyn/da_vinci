53f8591226401a11137d03e89f5424d9fc2ee0c6
Annika Brundyn <annikabrundyn1@gmail.com>
2021-04-09 16:18:00 -0400

    fix

--- src/predict/predict_unstacked.py
+++ src/predict/predict_unstacked.py
@@ -30,28 +30,6 @@ def concat_left_right(img, preds, num_frames, stacked=False):
     return left_and_right
 
 
-def ffmpeg_concat_videos():
-    stringa = "ffmpeg -i \"concat:"
-    elenco_video = glob.glob("*.mp4")
-    elenco_file_temp = []
-    for f in elenco_video:
-        file = "temp" + str(elenco_video.index(f) + 1) + ".ts"
-        os.system("ffmpeg -i " + f + " -c copy -bsf:v h264_mp4toannexb -f mpegts " + file)
-        elenco_file_temp.append(file)
-    print(elenco_file_temp)
-    for f in elenco_file_temp:
-        stringa += f
-        if elenco_file_temp.index(f) != len(elenco_file_temp) - 1:
-            stringa += "|"
-        else:
-            stringa += "\" -c copy  -bsf:a aac_adtstoasc output.mp4"
-    print(stringa)
-    os.system(stringa)
-
-
-
-
-
 if __name__ == "__main__":
     # sets seed for numpy, torch, python.random and PYTHONHASHSEED
     pl.seed_everything(42)
