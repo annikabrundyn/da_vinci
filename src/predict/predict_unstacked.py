2c860796ff856f989a6363e9168b7c0ea21be6ff
Annika Brundyn <annikabrundyn1@gmail.com>
2021-04-09 13:58:56 -0400

    fix

--- src/predict/predict_unstacked.py
+++ src/predict/predict_unstacked.py
@@ -4,6 +4,8 @@ import torch
 from tqdm import tqdm
 import torchvision
 
+import glob
+
 from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
@@ -27,6 +29,27 @@ def concat_left_right(img, preds, num_frames, stacked=False):
     return left_and_right
 
 
+def ffmpeg_concat_videos():
+    stringa = "ffmpeg -i \"concat:"
+    elenco_video = glob.glob("*.mp4")
+    elenco_file_temp = []
+    for f in elenco_video:
+        file = "temp" + str(elenco_video.index(f) + 1) + ".ts"
+        os.system("ffmpeg -i " + f + " -c copy -bsf:v h264_mp4toannexb -f mpegts " + file)
+        elenco_file_temp.append(file)
+    print(elenco_file_temp)
+    for f in elenco_file_temp:
+        stringa += f
+        if elenco_file_temp.index(f) != len(elenco_file_temp) - 1:
+            stringa += "|"
+        else:
+            stringa += "\" -c copy  -bsf:a aac_adtstoasc output.mp4"
+    print(stringa)
+    os.system(stringa)
+
+
+
+
 
 if __name__ == "__main__":
     # sets seed for numpy, torch, python.random and PYTHONHASHSEED
@@ -42,7 +65,7 @@ if __name__ == "__main__":
     parser.add_argument("--freq", required=True, type=int, help="how frequently to save video snippets")
     parser.add_argument("--max_frame_exp", type=int, default=10)
     parser.add_argument("--fps", type=int, default=18)
-    parser.add_argument("--video_format", type=str, default='.avi')
+    parser.add_argument("--video_format", type=str, default='avi')
     parser.add_argument("--stacked", action="store_true")
 
     args = parser.parse_args()
